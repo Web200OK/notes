2dd2cf86fed478ce3e7600ca4184227eb988a542
myname <1691929357@qq.com>
2022-01-18 17:54:53 +0800

    前端笔记

--- Data-Structure/算法题/获取url中的字符串参数.js
+++
@@ -1,6 +0,0 @@
-let url = "https：//○△×□.cn/?●=▲×■＆○=△×□&a=1"
-        let reg = /(?<=y)\?/
-        let result = url.match(reg).map((item)=>{
-            return [].concat(item)
-        })
-        console.log(result)
